c0009763334610de6b8bd34f68fbe8641d9b3206
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2021-11-01 13:03:55 -0400

    Finished day 1.

--- Day1.playground/Contents.swift
+++ Day1.playground/Contents.swift
@@ -3,3 +3,112 @@ import Cocoa
 // Day 1 - variables, constants, strings, and numbers
 
 var greeting = "Hello, playground"
+
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+let character = "Daphne"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+let managerName = "Michael Scott"
+let dogBreed = "Samoyed"
+let meaningOfLife = "How many roads must a man walk down?"
+
+var favoriteShow = "Orange is the New Black"
+favoriteShow = "The Good Place"
+favoriteShow = "Doctor Who"
+
+let actor = "Denzel Washington"
+
+let filename = "paris.jpg"
+let result = "⭐ You win! ⭐"
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+
+print(actor.count)
+
+let nameLength = actor.count
+print(nameLength)
+
+print(result.uppercased())
+
+print(movie.hasPrefix("A day"))
+
+print(filename.hasSuffix(".jpg"))
+
+var quote = "Change the world by being yourself"
+
+var burns = """
+The best laid schemes
+O’ mice and men
+Gang aft agley
+"""
+
+let score = 10
+
+let reallyBig = 1_00__00___00____00
+
+let lowerScore = score - 2
+let higherScore = score + 10
+let doubledScore = score * 2
+let squaredScore = score * score
+let halvedScore = score / 2
+print(score)
+
+var counter = 10
+counter = counter + 5
+
+counter += 5
+print(score)
+
+counter *= 2
+print(score)
+counter -= 10
+print(score)
+counter /= 2
+print(score)
+
+let number = 120
+print(number.isMultiple(of: 3))
+
+print(120.isMultiple(of: 3))
+
+let number2 = 0.1 + 0.2
+print(number2)
+
+let a = 1
+let b = 2.0
+
+let c = Double(a) + b
+
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+let int1 = 3
+
+var name = "Nicolas Cage"
+name = "John Travolta"
+
+var rating = 5.0
+rating *= 2
+
+var myInt = 1
+var myDouble = 1.0
+
+var meaningOfLife = 42
+
