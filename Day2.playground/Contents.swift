3b1e920e29c4dfc941f334fb580e0f017e197ad3
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2021-12-07 14:41:44 -0500

    Finished day 2

--- Day2.playground/Contents.swift
+++ Day2.playground/Contents.swift
@@ -3,3 +3,45 @@ import Cocoa
 // Day 2 - Booleans, string interpolation, and checkpoint 1
 
 var greeting = "Hello, playground"
+
+let goodDogs = true
+
+let isMultiple = 120.isMultiple(of: 3)
+
+var isAuthenticated = false
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+
+var gameOver = false
+print(gameOver)
+
+gameOver.toggle()
+print(gameOver)
+
+let firstPart = "Hello, "
+let secondPart = "world!"
+let greeting = firstPart + secondPart
+
+let people = "Haters"
+let action = "hate"
+let lyric = people + " gonna " + action
+print(lyric)
+
+let luggageCode = "1" + "2" + "3" + "4" + "5"
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let name = "Taylor"
+let age = 26
+let message = "Hello, my name is \(name) and I'm \(age) years old."
+print(message)
+
+let number = 11
+let missionMessage = "Apollo \(number) landed on the moon."
+
+print("5 x 5 is \(5 * 5)")
+
+var city = "Cardiff"
+var message = "Welcome to \(city)!"
